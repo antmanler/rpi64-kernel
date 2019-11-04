adff073da619877f6b51575b7903e2bcacc41a56
antmanler <antmanler@gmail.com>
2019-11-04 09:25:53 +0800

    [+] build with configs from sakaki

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-export DEFCONFIG=docker_kvm_rpi3_defconfig
+git submodule update
+cp -f defconfigs/sakaki/config defconfigs/sakaki_defconfig
+
+export DEFCONFIG=sakaki_defconfig
 docker-compose -p rpikernelbuild build
 docker-compose -p rpikernelbuild run builder
 docker-compose -p rpikernelbuild down
